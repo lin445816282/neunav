a19887814fd5806341a2692b0219b72c8901a59c
lin445816282 <lin445816282@gmail.com>
2026-07-04 10:33:52 +0800

    auto push by guardian

--- backend/main.py
+++ backend/main.py
@@ -348,4 +348,4 @@ async def serve_static(path: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8011)
+    uvicorn.run(app, host="0.0.0.0", port=8011, root_path="/neunav")
